84a748e8c8a05dc224bc93d8b0bed7deeb0a3b08
Anders Andersson <trezker@gmail.com>
2010-10-24 15:17:27 +0200

    Added platform normals in editor

--- editor/platform.cpp
+++ editor/platform.cpp
@@ -28,6 +28,11 @@ void Platform::Draw(Vector2 camera, ALLEGRO_COLOR c)
 		pe.x = cos(angle)*pn.x - sin(angle)*pn.y;
 		pe.y = sin(angle)*pn.x + cos(angle)*pn.y;
 		al_draw_line(ps.x - camera.x, ps.y - camera.y, pe.x - camera.x, pe.y - camera.y, c, 0);
+		
+		Vector2 middle = (pe+ps)/2-camera;
+		float a = (pe-ps).Angle()-M_PI/2;
+		al_draw_line(middle.x, middle.y, middle.x+cos(a)*5, middle.y+sin(a)*5, c, 0);
+		
 		ps = pe;
 	}
 }
